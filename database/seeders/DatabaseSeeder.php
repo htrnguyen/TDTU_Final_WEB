bb74debcff8930515fbf5d50740398a80fcbf7bc
luhtri <luhtri2109@gmail.com>
2024-04-30 18:03:20 +0700

    seeder for all tables

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class,
+            CategorySeeder::class,
+            OrderSeeder::class,
+            OrderDetailSeeder::class,
+            CouponSeeder::class,
+        ]);
+    }
+}
