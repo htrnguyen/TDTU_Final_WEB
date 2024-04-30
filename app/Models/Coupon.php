55c9ffd9b2846633d731c9c4dd1e4f23d5c79278
luhtri <luhtri2109@gmail.com>
2024-04-30 18:01:53 +0700

    add coupon models

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Coupon extends Model
 {
     use HasFactory;
+    
+    protected $primaryKey = 'coupon_id';
+    protected $fillable = ['coupon_code', 'discount', 'expiration_date'];
+    protected $dates = ['create_date', 'expiration_date'];
+    public $timestamps = false;
 }
