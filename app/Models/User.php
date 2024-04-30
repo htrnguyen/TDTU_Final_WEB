1ff958fd9b596cdc4f7d6b10ac57524df46ef1f6
luhtri <luhtri2109@gmail.com>
2024-04-30 15:17:19 +0700

    add eloquent models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,27 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'email', 'password', 'address', 'phone'];
+    protected $dates = ['created_at'];
+    public $timestamps = false;
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'user_id');
+    }
 }
