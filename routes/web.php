396e7ccd5d1cf6342742b9517f3e5abc1c2e23ae
luhtri <luhtri2109@gmail.com>
2024-05-04 04:41:13 +0700

    feat: login, logout and, register api

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,37 @@
 
 namespace App\Route;
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\ResetPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\MenController;
 
 
-// Route::view('/', 'client.home');
+Route::group(['namespace' => 'auth'], function () {
 
-Route::controller(UserController::class)->group(function () {
+    // Login
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+    Route::post('/logout', [LoginController::class, 'destroy']);
+    
 
-    Route::post('/login', 'store');
-    Route::get('/getUsers', 'index');
+    // Register
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::delete('/delete', [RegisterController::class, 'destroy']);
+
+
+    // Reset-password
+    Route::get('/reset-password', [ResetPasswordController::class, 'index'])->name('reset-password');
 });
 
+
+
+Route::get('/men', [MenController::class, 'index'])->name('men');
+
+Route::view('/', 'client.home')->name('home');
 Route::get('check-health', function () {
     return response()->json(['message' => 'Call URI from web'], Response::HTTP_OK);
 });
-
-Route::get('/', function () {
-    return view('client.home');
-})->name('home');
-
-
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::get('/reset-password', [ResetPasswordController::class, 'index'])->name('reset-password');
-Route::get('/men', [MenController::class, 'index'])->name('men');
