98b264c324bd2c77a0ab1761a1ea76bb49ff9cb2
luhtri <luhtri2109@gmail.com>
2024-05-08 04:56:11 +0700

    improve register and rename LoginController to SessionController

---
+++ app/Http/Controllers/Auth/SessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function index()
+    {
+        $breadcrumbs = [
+            'Home' => route('home'),
+            'Login' => null
+        ];
+
+        return view('auth.login', compact('breadcrumbs'));
+    }
+
+    public function store()
+    {
+        $credentials = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed
+            request()->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        // Authentication failed
+        throw ValidationException::withMessages([
+            'email' => 'Sorry, those credentials do not match our records.',
+        ]);
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect()->route('client.home');
+    }
+}
