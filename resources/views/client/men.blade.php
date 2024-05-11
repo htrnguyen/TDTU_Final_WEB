cc434b659f225074c6372649cb80166f3b4769c9
htyn-n <thuynngch@gmail.com>
2024-05-11 20:34:05 +0700

    range slide

--- resources/views/client/men.blade.php
+++ resources/views/client/men.blade.php
@@ -21,19 +21,11 @@
                             </div>
                         </div>
                         <div class="row slider-labels">
-                            <div class="col-xs-6 caption">
-                                <strong>Min:</strong> <span id="slider-range-value1"></span>
+                            <div class="col-sm-7 caption">
+                                <span id="slider-range-value1"></span>
                             </div>
-                            <div class="col-xs-6 text-right caption">
-                                <strong>Max:</strong> <span id="slider-range-value2"></span>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <form>
-                                    <input type="hidden" name="min-value" value="">
-                                    <input type="hidden" name="max-value" value="">
-                                </form>
+                            <div class="col-sm-5 caption">
+                                <span id="slider-range-value2"></span>
                             </div>
                         </div>
                     </div>
