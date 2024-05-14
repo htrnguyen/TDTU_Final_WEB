24b07a3cf9f689d8deadc63917933c19dd3dc711
htyn-n <thuynngch@gmail.com>
2024-05-14 21:27:41 +0700

    a

--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -12,7 +12,8 @@
                     <div class="row">
 
                         <div class="text-center">
-                            <form action="{{ route('account.avatar.update') }}" method="post" enctype="multipart/form-data" class="d-flex flex-column align-items-center">
+                            <form action="{{ route('account.avatar.update') }}" method="post" enctype="multipart/form-data"
+                                class="d-flex flex-column align-items-center">
                                 @csrf
                                 @method('PATCH')
                                 <div class="avatar-container">
@@ -103,126 +104,124 @@
                                             <input type="text" class="form-control " id="dob" placeholder="Empty"
                                                 value="{{ $user->date_of_birth ?? '' }}" readonly>
                                         </div>
+
                                         <div class="form-group pt-3">
                                             <label class="pb-2">Gender</label><br>
-                                            <div class="form-group pt-3">
-                                                <label class="pb-2">Gender</label><br>
-                                                <div class="form-check form-check-inline">
-                                                    <input class="form-check-input" type="radio" name="gender"
-                                                        id="male" value="male"
-                                                        :checked="{{ ($user->gender ?? '') === 'male' }}"> <label
-                                                        class="form-check-label" for="male">Male</label>
-                                                </div>
-                                                <div class="form-check form-check-inline">
-                                                    <input class="form-check-input" type="radio" name="gender"
-                                                        id="female" value="female"
-                                                        :checked="{{ ($user->gender ?? '') === 'female' }}"> <label
-                                                        class="form-check-label" for="female">Female</label>
-                                                </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="gender"
+                                                    id="male" value="male"
+                                                    :checked="{{ ($user->gender ?? '') === 'male' }}"> <label
+                                                    class="form-check-label" for="male">Male</label>
                                             </div>
-
-                                        </div>
-                                        <div class="form-group pt-3">
-                                            <label class="pb-2" for="phone">Phone Number</label>
-                                            <div class="input-group">
-                                                <input type="text" class="form-control " id="phone"
-                                                    placeholder="Empty" value="{{ $user->phone_number }}" readonly>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="gender"
+                                                    id="female" value="female"
+                                                    :checked="{{ ($user->gender ?? '') === 'female' }}"> <label
+                                                    class="form-check-label" for="female">Female</label>
                                             </div>
                                         </div>
-                                        <div class="form-group pt-3">
-                                            <label class="pb-2" for="email">Email</label>
-                                            <input type="email" class="form-control rounded rounded-4" id="email"
-                                                value="{{ $user->email }}" readonly>
-                                        </div>
-                                        <div class="form-group pt-3 d-flex justify-content-center">
-                                            <p class="btn btn-edit btn-dark me-3 px-4" id="editButton"
-                                                onclick="enableEdit()"><i class="fa-solid fa-pen-to-square "></i>Edit
-                                            </p>
-                                            <p class="btn btn-secondary rounded rounded-4" id="cancelButton"
-                                                style="display: none;" onclick="cancelEdit()">
-                                                Cancel
-                                            </p>
+
+                                    </div>
+                                    <div class="form-group pt-3">
+                                        <label class="pb-2" for="phone">Phone Number</label>
+                                        <div class="input-group">
+                                            <input type="text" class="form-control " id="phone"
+                                                placeholder="Empty" value="{{ $user->phone_number }}" readonly>
                                         </div>
                                     </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Change Password -->
-                <div class="card d-none" id="changePassword" style="background-color: #f7f7f79c">
-                    <div class="card-body">
-                        <div class="text-center">
-                            <h3 class="card-title fw-bold">Change your password</h3>
-                            <!-- Display email dynamically if needed -->
-                        </div>
-                        <form method="post" action="{{ route('password.change') }}">
-                            @csrf
-                            <div class="mt-3 mb-3">
-                                <input type="password" class="form-control py-2" name="old_password"
-                                    placeholder="Old password" required>
-                            </div>
-                            <div class="mt-3 mb-3">
-                                <input type="password" class="form-control py-2" name="password"
-                                    placeholder="New password" required>
-                            </div>
-                            <div class="mb-3">
-                                <input type="password" class="form-control mb-3 py-2" name="password_confirmation"
-                                    placeholder="Confirm new Password" required>
-                                <small class="form-text text-muted">
-                                    <ul>
-                                        <li>Minimum of 8 characters</li>
-                                        <li>Uppercase, lowercase letters, and one number</li>
-                                    </ul>
-                                </small>
-                            </div>
-                            <div class="d-flex justify-content-end">
-                                <button type="button" class="btn btn-outline-dark me-3">Cancel</button>
-                                <button type="submit" class="btn btn-dark">Save</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <!-- My Orders -->
-                <div class="card d-none" id="myOrders" style="background-color: #f7f7f79c ">
-                    <div class="card-body">
-                        <div class="row">
-                            <h3>Orders</h3>
-                            <div class="card mb-3 mt-3 no-border">
-                                <div class="row no-gutters align-items-center ">
-                                    <div class="col-md-3">
-                                        <img src="{{ asset('images/shoe1-black.jpg') }}" alt="Strutter shoe"
-                                            style="width:100%">
+                                    <div class="form-group pt-3">
+                                        <label class="pb-2" for="email">Email</label>
+                                        <input type="email" class="form-control rounded rounded-4" id="email"
+                                            value="{{ $user->email }}" readonly>
                                     </div>
-                                    <div class="col-md-3 py-2 px-2">
-                                        <p class="mt-2" style="font-weight: bold">Nike Air Max Pulse</p>
-                                        <p>Women's Shoes<br>
-                                            White<br>
-                                            Size
-                                            <select class="custom-select no-border readonly">
-                                                <option selected>S</option>
-                                                <option selected>M</option>
-                                                <option selected>L</option>
-                                                <option selected>XL</option>
-                                            </select>
+                                    <div class="form-group pt-3 d-flex justify-content-center">
+                                        <p class="btn btn-edit btn-dark me-3 px-4" id="editButton"
+                                            onclick="enableEdit()"><i class="fa-solid fa-pen-to-square "></i>Edit
                                         </p>
-                                        <button type="button" class="btn btn-light rounded rounded-5">
-                                            Rate
-                                        </button>
-                                        <a href="#" class="btn btn-dark rounded-5 ">Buy Again</a>
+                                        <p class="btn btn-secondary rounded rounded-4" id="cancelButton"
+                                            style="display: none;" onclick="cancelEdit()">
+                                            Cancel
+                                        </p>
                                     </div>
-                                    <div class="col-md-2">
-                                        <p style="font-weight: bold">$422.40</p>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <p>Quantity: <br>1</p>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <i class="btn btn-light rounded rounded-5"
-                                            style="font-weight: bold; color: rgb(1, 125, 1); ">Completed</i>
-                                    </div>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Change Password -->
+            <div class="card d-none" id="changePassword" style="background-color: #f7f7f79c">
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3 class="card-title fw-bold">Change your password</h3>
+                        <!-- Display email dynamically if needed -->
+                    </div>
+                    <form method="post" action="{{ route('password.change') }}">
+                        @csrf
+                        <div class="mt-3 mb-3">
+                            <input type="password" class="form-control py-2" name="old_password"
+                                placeholder="Old password" required>
+                        </div>
+                        <div class="mt-3 mb-3">
+                            <input type="password" class="form-control py-2" name="password" placeholder="New password"
+                                required>
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control mb-3 py-2" name="password_confirmation"
+                                placeholder="Confirm new Password" required>
+                            <small class="form-text text-muted">
+                                <ul>
+                                    <li>Minimum of 8 characters</li>
+                                    <li>Uppercase, lowercase letters, and one number</li>
+                                </ul>
+                            </small>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-outline-dark me-3">Cancel</button>
+                            <button type="submit" class="btn btn-dark">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- My Orders -->
+            <div class="card d-none" id="myOrders" style="background-color: #f7f7f79c ">
+                <div class="card-body">
+                    <div class="row">
+                        <h3>Orders</h3>
+                        <div class="card mb-3 mt-3 no-border">
+                            <div class="row no-gutters align-items-center ">
+                                <div class="col-md-3">
+                                    <img src="{{ asset('images/shoe1-black.jpg') }}" alt="Strutter shoe"
+                                        style="width:100%">
+                                </div>
+                                <div class="col-md-3 py-2 px-2">
+                                    <p class="mt-2" style="font-weight: bold">Nike Air Max Pulse</p>
+                                    <p>Women's Shoes<br>
+                                        White<br>
+                                        Size
+                                        <select class="custom-select no-border readonly">
+                                            <option selected>S</option>
+                                            <option selected>M</option>
+                                            <option selected>L</option>
+                                            <option selected>XL</option>
+                                        </select>
+                                    </p>
+                                    <button type="button" class="btn btn-light rounded rounded-5">
+                                        Rate
+                                    </button>
+                                    <a href="#" class="btn btn-dark rounded-5 ">Buy Again</a>
+                                </div>
+                                <div class="col-md-2">
+                                    <p style="font-weight: bold">$422.40</p>
+                                </div>
+                                <div class="col-md-2">
+                                    <p>Quantity: <br>1</p>
+                                </div>
+                                <div class="col-md-2">
+                                    <i class="btn btn-light rounded rounded-5"
+                                        style="font-weight: bold; color: rgb(1, 125, 1); ">Completed</i>
                                 </div>
                             </div>
                         </div>
@@ -230,6 +229,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <script>
         function previewImage(event) {
